0e1009724f7b3b26d303518d319aa2ae2acbbed3
Ryan Reyes <ryanjaytagolimotreyes@gmail.com>
2025-07-16 21:39:18 +0800

    t

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'dist/index.html',
+          dest: '.', // this means "copy to the root"
+          rename: '404.html'
+        }
+      ]
+    })
+  ],
   base: "/",
   resolve: {
     alias: {
