f5313b2e592b8e54c9aba6f6f4855272ed717015
Ryan Reyes <ryanjaytagolimotreyes@gmail.com>
2025-07-16 09:06:29 +0800

    y

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: './', 
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-  server: {
-    port: 5000,
-    host: true
-  }
 })
